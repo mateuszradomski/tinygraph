f639b46cd8dfc5407e9bcdd1f49e5d66de98cc08
Mateusz Radomski <mateusz.radomski@l2beat.com>
2023-05-20 12:55:12 +0200

    Refactor to a class

--- website/index.js
+++ website/index.js
@@ -1,15 +1,5 @@
 const svg = document.getElementById("main");
 
-const drawPolyline = (svg, points) => {
-    const pointsAttribValue = points.map(([x, y]) => `${x}, ${y}`).join(" ")
-
-    const polyline = document.createElementNS("http://www.w3.org/2000/svg", "polyline");
-    polyline.setAttribute("points", pointsAttribValue);
-    polyline.setAttribute("stroke", "pink");
-    polyline.setAttribute("fill", "none");
-    svg.appendChild(polyline)
-}
-
 const scale = 8;
 let points = [
     [scale * 0, 347],
@@ -118,7 +108,7 @@ function lerp(k0, k1, t) {
     return k0 + t * (k1 - k0)
 }
 
-const getInterpolatedY = (x, points) => {
+function getInterpolatedY(x, points) {
     let i = 0;
     for (i = 0; i < points.length; i++) {
         if (points[i][0] > x) { break; }
@@ -129,34 +119,50 @@ const getInterpolatedY = (x, points) => {
     return lerp(points[i - 1][1], points[i][1], (x - points[i - 1][0]) / Math.abs(points[i][0] - points[i - 1][0]))
 }
 
-const chartHoverLine = document.createElementNS("http://www.w3.org/2000/svg", "line");
-const chartHoverCircle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+class LineGraph {
+    constructor(svg) {
+        this.svg = svg;
+        this.hoverLine = document.createElementNS("http://www.w3.org/2000/svg", "line");
+        this.hoverCircle = document.createElementNS("http://www.w3.org/2000/svg", "circle");
 
-chartHoverLine.setAttribute("stroke", "grey");
-chartHoverCircle.setAttribute("stroke", "grey");
-chartHoverCircle.setAttribute("r", "3");
+        this.hoverLine.setAttribute("stroke", "grey");
+        this.hoverCircle.setAttribute("stroke", "grey");
+        this.hoverCircle.setAttribute("r", "3");
 
-svg.appendChild(chartHoverLine)
-svg.appendChild(chartHoverCircle)
+        this.svg.appendChild(this.hoverLine)
+        this.svg.appendChild(this.hoverCircle)
 
-svg.addEventListener("mousemove", (e) => {
-    chartHoverLine.setAttribute("x1", `${e.offsetX}`)
-    chartHoverLine.setAttribute("y1", "0")
-    chartHoverLine.setAttribute("x2", `${e.offsetX}`)
-    chartHoverLine.setAttribute("y2", "600")
+        svg.addEventListener("mousemove", (e) => {
+            this.hoverLine.setAttribute("x1", `${e.offsetX}`)
+            this.hoverLine.setAttribute("y1", "0")
+            this.hoverLine.setAttribute("x2", `${e.offsetX}`)
+            this.hoverLine.setAttribute("y2", "600")
 
-    chartHoverCircle.setAttribute("cx", `${e.offsetX}`)
-    chartHoverCircle.setAttribute("cy", `${getInterpolatedY(e.offsetX, points)}`)
-})
+            this.hoverCircle.setAttribute("cx", `${e.offsetX}`)
+            this.hoverCircle.setAttribute("cy", `${getInterpolatedY(e.offsetX, points)}`)
+        })
 
-svg.addEventListener("mouseenter", (_) => {
-    chartHoverLine.setAttribute("class", "")
-    chartHoverCircle.setAttribute("class", "")
-})
+        svg.addEventListener("mouseenter", (_) => {
+            this.hoverLine.setAttribute("class", "")
+            this.hoverCircle.setAttribute("class", "")
+        })
 
-svg.addEventListener("mouseleave", (_) => {
-    chartHoverLine.setAttribute("class", "hidden")
-    chartHoverCircle.setAttribute("class", "hidden")
-})
+        svg.addEventListener("mouseleave", (_) => {
+            this.hoverLine.setAttribute("class", "hidden")
+            this.hoverCircle.setAttribute("class", "hidden")
+        })
+    }
 
-drawPolyline(svg, points)
+    draw(points) {
+        const pointsAttribValue = points.map(([x, y]) => `${x}, ${y}`).join(" ")
+
+        const polyline = document.createElementNS("http://www.w3.org/2000/svg", "polyline");
+        polyline.setAttribute("points", pointsAttribValue);
+        polyline.setAttribute("stroke", "pink");
+        polyline.setAttribute("fill", "none");
+        this.svg.appendChild(polyline)
+    }
+}
+
+const testGraph = new LineGraph(svg)
+testGraph.draw(points)
