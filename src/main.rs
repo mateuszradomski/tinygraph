3434f1a589d7985d556e1bf7992e97a0faa3d70e
Mateusz Radomski <mateusz.radomski@l2beat.com>
2023-05-29 00:15:46 +0200

    Add compression to output file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use std::{
     fs::File,
+    io::{Cursor, Read},
     thread::sleep,
     time::{Duration, SystemTime, UNIX_EPOCH},
 };
@@ -9,6 +10,8 @@ use sysinfo::{ComponentExt, DiskExt, NetworkExt, System, SystemExt};
 use std::io::{stdout, Write};
 
 use clap::Parser;
+
+use libdeflater::{CompressionLvl, Compressor, Decompressor};
 
 mod tgph_format;
 use tgph_format::TGPH;
@@ -27,15 +30,53 @@ struct Args {
     timeout_period: u64,
 }
 
-fn main() {
+fn decompress<R: Read>(stream: &mut R) -> Result<Vec<u8>, std::io::Error> {
+    let mut gz_data = Vec::new();
+    stream.read_to_end(&mut gz_data)?;
+
+    if gz_data.len() < 10 {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::InvalidData,
+            "gz data is too short (for magic bytes + footer",
+        ));
+    }
+
+    let isize = {
+        let isize_start = gz_data.len() - 4;
+        let isize_bytes = &gz_data[isize_start..];
+        let mut ret: u32 = isize_bytes[0] as u32;
+        ret |= (isize_bytes[1] as u32) << 8;
+        ret |= (isize_bytes[2] as u32) << 16;
+        ret |= (isize_bytes[3] as u32) << 24;
+        ret as usize
+    };
+
+    let mut decompressor = Decompressor::new();
+    let mut outbuf = Vec::new();
+    outbuf.resize(isize, 0);
+    decompressor.gzip_decompress(&gz_data, &mut outbuf).unwrap();
+    Ok(outbuf)
+}
+
+fn main() -> Result<(), std::io::Error> {
     let args = Args::parse();
 
     let mut sys = System::new_all();
     let mut stdout = stdout();
 
+    if !args.output_path.ends_with(".gz") {
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::InvalidData,
+            "Expected the output file to be a gzip file with \".gz\" ending",
+        ));
+    }
+
     let mut tgph = match File::open(args.output_path.clone()) {
-        Ok(mut file) => {
-            let mut res = TGPH::deserialize_from(&mut file).unwrap();
+        Ok(mut compressed_file) => {
+            let decompressed = decompress(&mut compressed_file).unwrap();
+            let mut cursor = Cursor::new(decompressed);
+
+            let mut res = TGPH::deserialize_from(&mut cursor).unwrap();
             res.entry_limit = args.entry_limit;
             res
         }
@@ -121,8 +162,23 @@ fn main() {
             "Hostname",
         );
 
-        let mut output_file = File::create(args.output_path.clone()).unwrap();
-        tgph.serialize_into(&mut output_file).unwrap();
+        let mut output_buffer = Vec::new();
+        tgph.serialize_into(&mut output_buffer).unwrap();
+
+        let compressed_data = {
+            let mut compressor = Compressor::new(CompressionLvl::default());
+            let max_sz = compressor.gzip_compress_bound(output_buffer.len());
+            let mut compressed_data = Vec::new();
+            compressed_data.resize(max_sz, 0);
+            let actual_sz = compressor
+                .gzip_compress(&output_buffer, &mut compressed_data)
+                .unwrap();
+            compressed_data.resize(actual_sz, 0);
+            compressed_data
+        };
+
+        let mut output_file = File::create(args.output_path.clone())?;
+        output_file.write_all(&compressed_data)?;
 
         points_saved += 1;
 
